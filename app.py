6dd426c5ef7ab7547d27b25a7c5baf545764cdd1
drahmetacik <a.acik@icloud.com>
2024-12-10 10:56:35 +0000

    refactor: update GraphQL schema to use graphene and improve user data resolution

--- app.py
+++ app.py
@@ -1,37 +1,42 @@
-from graphene import ObjectType, String, Field, List, Schema
+import graphene
 from type import User
 from mutation import CreateUser, UpdateUser, DeleteUser
-from query import Query
 
+# Hypothetical data store
+data_store = {
+    "1": {"id": "1", "name": "Ahmet Doe", "email": "ahmet@gmail.com", "password": "123456"},
+    "2": {"id": "2", "name": "Mehmet Doe", "email": "mehmet@gmail.com", "password": "123456"}
+}
 
-class MyMutations(ObjectType):
+class MyMutations(graphene.ObjectType):
     create_user = CreateUser.Field()
     update_user = UpdateUser.Field()
     delete_user = DeleteUser.Field()
 
-
-class MyQuery(ObjectType):
-    user = Field(User, id=String(required=True))
-    get_user = Field(User, id=String(required=True))
-    get_users = List(User)
+class MyQuery(graphene.ObjectType):
+    user = graphene.Field(User, id=graphene.String(required=True))
+    get_user = graphene.Field(User, id=graphene.String(required=True))
+    get_users = graphene.List(User)
 
     def resolve_user(self, info, id):
         # Fetch user from your data store here
-        return User(id=id, name="Example", email="example@example.com", password="password")
+        user_data = data_store.get(id)
+        if user_data:
+            return User(**user_data)
+        return None
 
     def resolve_get_user(self, info, id):
         # Fetch user from your data store here
-        return User(id=id, name="Example", email="example@example.com", password="password")
+        user_data = data_store.get(id)
+        if user_data:
+            return User(**user_data)
+        return None
 
     def resolve_get_users(self, info):
         # Fetch all users from your data store here
-        return [
-            User(id="1", name="Ahmet Doe", email="ahmet@gmail.com", password="123456"),
-            User(id="2", name="Mehmet Doe", email="mehmet@gmail.com", password="123456")
-        ]
+        return [User(**user_data) for user_data in data_store.values()]
 
-
-schema = Schema(query=MyQuery, mutation=MyMutations)
+schema = graphene.Schema(query=MyQuery, mutation=MyMutations)
 
 def execute_and_print(query):
     result = schema.execute(query)
@@ -69,6 +74,7 @@ mutation {
 }
 """
 )
+
 execute_and_print(
     """
 mutation {
--- query.py
+++ query.py
@@ -1,22 +1,31 @@
 import graphene
 from type import User
 
+# Hypothetical data store
+data_store = {
+    "1": {"id": "1", "name": "Ahmet Doe", "email": "ahmet@gmail.com", "password": "123456"},
+    "2": {"id": "2", "name": "Mehmet Doe", "email": "mehmet@gmail.com", "password": "123456"}
+}
+
 class Query(graphene.ObjectType):
-    user = graphene.Field(User, id=graphene.String())
-    get_user = graphene.Field(User, id=graphene.String())
+    user = graphene.Field(User, id=graphene.String(required=True))
+    get_user = graphene.Field(User, id=graphene.String(required=True))
     get_users = graphene.List(User)
 
     def resolve_user(self, info, id):
         # Fetch user from your data store here
-        return User(id=id, name="Example", email="example@example.com", password="password")
+        user_data = data_store.get(id)
+        if user_data:
+            return User(**user_data)
+        return None
 
     def resolve_get_user(self, info, id):
         # Fetch user from your data store here
-        return User(id=id, name="Example", email="example@example.com", password="password")
+        user_data = data_store.get(id)
+        if user_data:
+            return User(**user_data)
+        return None
 
     def resolve_get_users(self, info):
         # Fetch all users from your data store here
-        return [
-            User(id="1", name="Ahmet Doe", email="ahmet@gmail.com", password="123456"),
-            User(id="2", name="Mehmet Doe", email="mehmet@gmail.com", password="123456")
-        ]
+        return [User(**user_data) for user_data in data_store.values()]
